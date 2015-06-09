025e22c7b3dc3dbe305ac92568d2d48425fa4fc3
Matias Fito <matias.fito@outlook.com>
2015-06-09 20:15:22 -0300

    TorneoORM

---
+++ DataModel/TorneoORM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataModel
+{
+    public class TorneoORM
+    {
+        private EpicLeagueEntities contexto = new EpicLeagueEntities();
+
+        public void ListarTorneos()
+        {
+            var torneos = from t in contexto.Torneo
+                          select t;
+
+            foreach (Torneo torneo in torneos)
+            {
+                Console.WriteLine(torneo.Nombre);
+            }
+        }
+    }
+}
