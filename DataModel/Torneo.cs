d65edbb4716ef51e153c0f60136d6aa0f2436688
Matias Fito <matias.fito@outlook.com>
2015-06-14 19:35:41 -0300

    Torneo 100% funcional

--- DataModel/Torneo.cs
+++ DataModel/Torneo.cs
@@ -20,11 +20,62 @@ namespace DataModel
             }
         }
 
+        public int CantidadDeTorneos
+        {
+            get
+            {
+                return this.ListarTodosLosTorneos().Count;
+            }
+        }        
+
         public List<Torneo> ListarTodosLosTorneos()
         {
             var torneos = from t in Entidades.Torneo
                           select t;
             return torneos.ToList<Torneo>();
+        }
+
+        public List<Torneo> BuscarTorneoPorNombre(string nombre)
+        {
+            List<Torneo> torneos;
+
+            if (nombre == string.Empty)
+            {
+                torneos = this.ListarTodosLosTorneos();
+            }
+            else
+            {
+                var consulta = from t in Entidades.Torneo
+                               where t.Nombre.Contains(nombre)
+                               select t;
+                torneos = consulta.ToList<Torneo>();
+            }
+            return torneos;
+        }
+
+        public Torneo AgregarTorneo(string nombre, bool activo)
+        {
+            Torneo torneo = new Torneo();
+            torneo.Id = this.CantidadDeTorneos;
+            torneo.Nombre = nombre;
+            torneo.Activo = activo;
+
+            Entidades.Torneo.AddObject(torneo);
+            Entidades.SaveChanges();
+            return torneo;
+        }
+
+        public int EliminarTorneo(int IdTorneo)
+        {
+            int resultado = 0;
+            System.Data.EntityKey key = new System.Data.EntityKey("EpicLeagueEntities.Torneo", "Id", IdTorneo);
+            object torneoParaEliminar;
+            if (Entidades.TryGetObjectByKey(key, out torneoParaEliminar))
+            {
+                Entidades.DeleteObject(torneoParaEliminar);
+                resultado = Entidades.SaveChanges();
+            }
+            return resultado;
         }
     }
 }
--- Tests/TorneoTest.cs
+++ Tests/TorneoTest.cs
@@ -26,5 +26,30 @@ namespace Tests
             List<Torneo> torneos = torneo.ListarTodosLosTorneos();
             Assert.AreEqual(2, torneos.Count);
         }
+
+        [TestMethod]
+        public void BuscarTorneoClausuraDeberiaDevolverElTorneoClausura()
+        {
+            List<Torneo> torneos = torneo.BuscarTorneoPorNombre("Clausura");
+            Assert.IsTrue("Torneo Clausura" == torneos[0].Nombre);
+        }
+
+        [TestMethod]
+        public void AgregarTorneoDeberiaAgregarUnTorneo()
+        {
+            int cantidadDeTorneosAntesDeAgregarUno = torneo.CantidadDeTorneos;
+            torneo.AgregarTorneo("Prueba", true);
+            Assert.AreEqual(cantidadDeTorneosAntesDeAgregarUno + 1, torneo.CantidadDeTorneos);
+        }
+
+        [TestMethod]
+        public void EliminarTorneoDeberiaEliminarUnTorneo()
+        {
+            int cantidadDeTorneosAntesDeEliminarUno = torneo.CantidadDeTorneos;
+            int IdTorneoAEliminar = torneo.CantidadDeTorneos - 1;
+            torneo.EliminarTorneo(IdTorneoAEliminar);
+            Assert.AreEqual(cantidadDeTorneosAntesDeEliminarUno - 1, torneo.CantidadDeTorneos);
+        }
+
     }
 }
